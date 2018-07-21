46e8cd97b19835b952652b6dec57e7b53f53646d
Bang <forresti3979@gmail.com>
2018-07-21 15:17:09 -0700

    Fin Sign In UI & minor fix

--- client/app/src/main/java/com/example/junhyuk/sjsu_client/MainActivity.java
+++ client/app/src/main/java/com/example/junhyuk/sjsu_client/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.junhyuk.sjsu_client;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-    public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-        LoginActivity login;
+    // LoginActivity login;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-
-
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.loginActivity);
-            login = new LoginActivity();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // login = new LoginActivity();
 
-            //setContentView(R.layout.activity_main);
-        }
-
+        /* Sign Up Button Click Event : Page Redirect*/
+        findViewById(R.id.joinBtn).setOnClickListener(
+            new Button.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent_act = new Intent(getApplicationContext(), SignUpActivity.class);
+                    startActivity(intent_act);
+                }
+            }
+        );
     }
+}
