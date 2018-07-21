e93afa2f29180b8f6fbc3705ce10954dbb1c84e0
Bang <forresti3979@gmail.com>
2018-07-21 15:19:12 -0700

    Rename loginActivity.xml to login_activity.xml
    Due to Error:
        'A' is not a valid file-based resource name character:
            File-based resource names must contain only lowercase a-z, 0-9, or underscore

--- client/app/src/main/java/com/example/junhyuk/sjsu_client/LoginActivity.java
+++ client/app/src/main/java/com/example/junhyuk/sjsu_client/LoginActivity.java
@@ -1,15 +1,10 @@
 package com.example.junhyuk.sjsu_client;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
 public class LoginActivity extends MainActivity {
@@ -22,7 +17,7 @@ public class LoginActivity extends MainActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginActivity);
+        setContentView(R.layout.login_activity);
 
         username = (EditText) findViewById(R.id.emailInput);
         password = (EditText) findViewById(R.id.pwInput);
