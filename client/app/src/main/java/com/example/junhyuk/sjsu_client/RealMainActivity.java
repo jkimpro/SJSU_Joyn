93687933ecb7afab1612e059ba35663fe9819b07
Junhyuk Kim <okjcd123@naver.com>
2018-07-31 00:15:12 -0700

    add friend

--- client/app/src/main/java/com/example/junhyuk/sjsu_client/RealMainActivity.java
+++ client/app/src/main/java/com/example/junhyuk/sjsu_client/RealMainActivity.java
@@ -35,6 +35,12 @@ public class RealMainActivity extends AppCompatActivity{
             }
         });
 
+        findViewById(R.id.friendImage1).setOnClickListener(friendButtonListener);
+        findViewById(R.id.friendImage2).setOnClickListener(friendButtonListener);
+        findViewById(R.id.friendImage3).setOnClickListener(friendButtonListener);
+        findViewById(R.id.friendImageElse1).setOnClickListener(friendButtonListener);
+        findViewById(R.id.friendImageElse2).setOnClickListener(friendButtonListener);
+
         currentApiVersion = android.os.Build.VERSION.SDK_INT;
 
         final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -85,5 +91,12 @@ public class RealMainActivity extends AppCompatActivity{
         }
     }
 
+    Button.OnClickListener friendButtonListener =
+            new Button.OnClickListener(){
+                public void onClick(View v) {
+                    Intent intent = new Intent(RealMainActivity.this, FriendViewActivity.class);
+                    startActivity(intent);
+                }
+            };
 
 }
--- client/app/src/main/java/com/example/junhyuk/sjsu_client/SelectRoomActivity.java
+++ client/app/src/main/java/com/example/junhyuk/sjsu_client/SelectRoomActivity.java
@@ -3,6 +3,7 @@ package com.example.junhyuk.sjsu_client;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,6 +11,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class SelectRoomActivity extends AppCompatActivity {
+
+
+    private int currentApiVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,73 @@ public class SelectRoomActivity extends AppCompatActivity {
         ImageView imageView = (ImageView)findViewById(R.id.back);
         imageView.setImageBitmap(bitmapImage);
 
+        findViewById(R.id.friendImage1).setOnClickListener(friendButtonListener);
+        findViewById(R.id.friendImage2).setOnClickListener(friendButtonListener);
+        findViewById(R.id.friendImage3).setOnClickListener(friendButtonListener);
+        findViewById(R.id.friendImageElse1).setOnClickListener(friendButtonListener);
+        findViewById(R.id.friendImageElse2).setOnClickListener(friendButtonListener);
+
+
+        currentApiVersion = android.os.Build.VERSION.SDK_INT;
+
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        // This work only for android 4.4+
+        if(currentApiVersion >= Build.VERSION_CODES.KITKAT)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+
+            // Code below is to handle presses of Volume up or Volume down.
+            // Without this, after pressing volume buttons, the navigation bar will
+            // show up and won't hide
+            final View decorView = getWindow().getDecorView();
+            decorView
+                    .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener()
+                    {
+
+                        @Override
+                        public void onSystemUiVisibilityChange(int visibility)
+                        {
+                            if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                            {
+                                decorView.setSystemUiVisibility(flags);
+                            }
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        if(currentApiVersion >= Build.VERSION_CODES.KITKAT && hasFocus)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 
     Button.OnClickListener joinCloseButton =
             new Button.OnClickListener(){
                 public void onClick(View v) {
                     Intent intent = new Intent(SelectRoomActivity.this, RealMainActivity.class);
+                    startActivity(intent);
+                }
+            };
+
+    Button.OnClickListener friendButtonListener =
+            new Button.OnClickListener(){
+                public void onClick(View v) {
+                    Intent intent = new Intent(SelectRoomActivity.this, FriendViewActivity.class);
                     startActivity(intent);
                 }
             };
