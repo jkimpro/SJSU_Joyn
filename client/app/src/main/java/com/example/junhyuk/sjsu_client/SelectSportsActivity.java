ba5fc18ecf756cffbd913e2ec37f15347fa64336
wooseong <lisa960310@naver.com>
2018-08-02 16:12:14 +0900

    real_main까지 끝

--- client/app/src/main/java/com/example/junhyuk/sjsu_client/SelectSportsActivity.java
+++ client/app/src/main/java/com/example/junhyuk/sjsu_client/SelectSportsActivity.java
@@ -3,8 +3,11 @@ package com.example.junhyuk.sjsu_client;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,13 +29,17 @@ public class SelectSportsActivity extends AppCompatActivity {
      12 = Surfing
     * */
     boolean [] isSelected;
-
+    ActionBar actionBar;
+    private int currentApiVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sportschioce);
+
+        hideBar();
+        getToolbar();
 
         isSelected = new boolean[12];
         Log.e("SelectSportsActivity","Launch");
@@ -217,6 +224,49 @@ public class SelectSportsActivity extends AppCompatActivity {
 
                 }
             };
+
+    public void hideBar() {
+        //상하단바 없애기
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        currentApiVersion = android.os.Build.VERSION.SDK_INT;
+        // This work only for android 4.4+
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+
+            getWindow().getDecorView().setSystemUiVisibility(flags);
+
+            // Code below is to handle presses of Volume up or Volume down.
+            // Without this, after pressing volume buttons, the navigation bar will
+            // show up and won't hideㅁ
+            final View decorView = getWindow().getDecorView();
+            decorView
+                    .setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+
+                        @Override
+                        public void onSystemUiVisibilityChange(int visibility) {
+                            if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                                decorView.setSystemUiVisibility(flags);
+                            }
+                        }
+                    });
+        }
+    }
+
+    public void getToolbar(){
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        actionBar = getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp); //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
+        actionBar.setTitle(null);
+    }
 
 
 }
--- client/app/src/main/java/com/example/junhyuk/sjsu_client/SignUpActivity.java
+++ client/app/src/main/java/com/example/junhyuk/sjsu_client/SignUpActivity.java
@@ -184,7 +184,7 @@ public class SignUpActivity extends AppCompatActivity {
         actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
         actionBar.setDisplayShowTitleEnabled(false);
         actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
-        actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp); //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp); //뒤로가기 버튼을 본인이 만든 아이콘으로 하기 위해 필요
         actionBar.setTitle(null);
     }
     @Override
